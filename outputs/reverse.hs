6de2a3043ad35d55a4d7452289816e497730ffd4
KenIshii <riguveda44@gmail.com>
2025-02-24 23:51:00 +0900

    Update reverse.hs

--- outputs/reverse.hs
+++ outputs/reverse.hs
@@ -1,3 +1,5 @@
 reverse' :: [a] -> [a]
-reverse' [] = []
-reverse' (x:xs) = reverse' xs ++ [x]
+-- reverse' [] = []
+-- reverse' (x:xs) = reverse' xs ++ [x]
+reverse' = foldl (flip (:)) []
+-- reverse' = foldl (\acc x -> x : acc) [] とも書けるが、flipを使うことで引数の順序を逆にすることができる
